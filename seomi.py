247b03b672251bc1a1e4581bd5399d5143962c54
dolphinxyz <robimalco@gmail.com>
2023-03-28 10:16:34 +0200

    simpify code, better word count, better paragraph count

--- seomi.py
+++ seomi.py
@@ -1,29 +1,24 @@
 import os
 import sys
-import json
 import datetime
 import psycopg2
 import requests
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
-from urllib.parse import urlparse, urljoin
+from utilities import *
 
 timestamp = datetime.datetime.now()
 
 
 def main(sitemap_url):
 
-    # Download the sitemap content
     response = requests.get(sitemap_url)
-
-    # Parse the sitemap XML
     root = ET.fromstring(response.content)
 
     urls = []
     for loc in root.findall('.//{http://www.sitemaps.org/schemas/sitemap/0.9}loc'):
         urls.append(loc.text)
 
-    # Set up the PostgreSQL connection
     conn = psycopg2.connect(
         dbname=os.environ['DB_NAME'],
         user=os.environ['DB_USER'],
@@ -33,91 +28,58 @@ def main(sitemap_url):
     )
     cursor = conn.cursor()
 
-    # Process each URL
     for url in urls:
         process_url(url, cursor, conn)
 
-    # Close the PostgreSQL connection
     cursor.close()
     conn.close()
 
 
 def process_url(url, cursor, conn):
-    # Download the page content
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Scrape the data
     item = {}
     item['url'] = url
-    item['title'] = soup.title.string if soup.title else None
-    item['meta_description'] = soup.find('meta', attrs={'name': 'description'})['content'] if soup.find('meta', attrs={'name': 'description'}) else None
-    item['h1'] = soup.h1.text if soup.h1 else None
-    item['h2'] = json.dumps([h2.text for h2 in soup.find_all('h2')])
-    item['internal_links'] = [a['href'] for a in soup.find_all('a', href=True) if urlparse(a['href']).netloc == urlparse(url).netloc]
-    item['external_links'] = json.dumps([a['href'] for a in soup.find_all('a', href=True) if urlparse(a['href']).netloc != urlparse(url).netloc])
-    item['url_length'] = len(url)
-    item['title_length'] = len(item['title']) if item['title'] else None
-    item['meta_description_length'] = len(item['meta_description']) if item['meta_description'] else None
-    item['h1_length'] = len(item['h1']) if item['h1'] else None
-    item['h2_length'] = json.dumps([len(i) for i in item['h2'].split('"')])
-    item['count_paragraphs'] = len(soup.find_all('p'))
-    item['response_time'] = response.elapsed.total_seconds()
-    item['status'] = response.status_code
-    item['word_count'] = len(soup.get_text().split())
-    item['page_size'] = len(response.content)
-    item['text_ratio'] = item['word_count'] / item['page_size']
-    item['canonical_url'] = soup.find('link', attrs={'rel': 'canonical'})['href'] if soup.find('link', attrs={'rel': 'canonical'}) else None
-    item['meta_robots'] = soup.find('meta', attrs={'name': 'robots'})['content'] if soup.find('meta', attrs={'name': 'robots'}) else None
-    item['image_alt_attributes'] = json.dumps([img['alt'] if 'alt' in img.attrs else None for img in soup.find_all('img')])
-    item['structured_data'] = json.dumps([script.string for script in soup.find_all('script', attrs={'type': 'application/ld+json'})])
-    item['broken_links'] = []  # This requires additional logic to identify broken links
-    item['language_tags'] = soup.html['lang'] if 'lang' in soup.html.attrs else None
+    item['title'] = extract_title(soup)
+    item['meta_description'] = extract_meta_description(soup)
+    item['h1'] = extract_h1(soup)
+    item['h2'] = extract_h2(soup)
+    item['internal_links'] = extract_internal_links(soup, url)
+    item['external_links'] = extract_external_links(soup, url)
+    item['url_length'] = extract_url_length(url)
+    item['title_length'] = extract_title_length(item['title'])
+    item['meta_description_length'] = extract_meta_description_length(item['meta_description'])
+    item['h1_length'] = extract_h1_length(item['h1'])
+    item['h2_length'] = extract_h2_length(item['h2'])
+    item['count_paragraphs'] = extract_count_paragraphs(soup)
+    item['response_time'] = extract_response_time(response)
+    item['status'] = extract_status(response)
+    item['word_count'] = extract_word_count(soup)
+    item['page_size'] = extract_page_size(response)
+    item['text_ratio'] = extract_text_ratio(item['word_count'], item['page_size'])
+    item['canonical_url'] = extract_canonical_url(soup)
+    item['meta_robots'] = extract_meta_robots(soup)
+    item['image_alt_attributes'] = extract_image_alt_attributes(soup)
+    item['structured_data'] = extract_structured_data(soup)
+    item['broken_links'] = extract_broken_links(soup, url)
+    item['language_tags'] = extract_language_tags(soup)
     item['timestamp'] = timestamp
 
-    # Save the data to the
     save_to_postgresql(item, cursor, conn)
 
+
 def save_to_postgresql(item, cursor, conn):
-    query = """
-        INSERT INTO seo_data (
-            url, title, meta_description, h1, h2, internal_links, external_links,
-            url_length, title_length, meta_description_length, h1_length, h2_length, count_paragraphs,
-            response_time, status, word_count, page_size, text_ratio, canonical_url,
-            meta_robots, image_alt_attributes, structured_data, broken_links, language_tags, timestamp
-        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    columns = ', '.join(item.keys())
+    placeholders = ', '.join(['%s'] * len(item))
+    query = f"""
+        INSERT INTO seo_data ({columns})
+        VALUES ({placeholders});
     """
-
-    data = (
-        item['url'],
-        item['title'],
-        item['meta_description'],
-        item['h1'],
-        item['h2'],
-        item['internal_links'],
-        item['external_links'],
-        item['url_length'],
-        item['title_length'],
-        item['meta_description_length'],
-        item['h1_length'],
-        item['h2_length'],
-        item['count_paragraphs'],
-        item['response_time'],
-        item['status'],
-        item['word_count'],
-        item['page_size'],
-        item['text_ratio'],
-        item['canonical_url'],
-        item['meta_robots'],
-        item['image_alt_attributes'],
-        item['structured_data'],
-        item['broken_links'],
-        item['language_tags'],
-        item['timestamp']
-    )
-
+    data = tuple(item.values())
     cursor.execute(query, data)
     conn.commit()
+
 
 if __name__ == '__main__':
     sitemap_url = sys.argv[1]
---
+++ utilities.py
@@ -0,0 +1,102 @@
+import re
+import json
+from urllib.parse import urlparse
+
+
+def extract_title(soup):
+    return soup.title.string if soup.title else None
+
+
+def extract_meta_description(soup):
+    meta_tag = soup.find('meta', attrs={'name': 'description'})
+    return meta_tag['content'] if meta_tag else None
+
+
+def extract_h1(soup):
+    return soup.h1.text if soup.h1 else None
+
+
+def extract_h2(soup):
+    return json.dumps([h2.text for h2 in soup.find_all('h2')])
+
+
+def extract_internal_links(soup, url):
+    return [a['href'] for a in soup.find_all('a', href=True) if urlparse(a['href']).netloc == urlparse(url).netloc]
+
+
+def extract_external_links(soup, url):
+    return json.dumps([a['href'] for a in soup.find_all('a', href=True) if urlparse(a['href']).netloc != urlparse(url).netloc])
+
+
+def extract_url_length(url):
+    return len(url)
+
+
+def extract_title_length(title):
+    return len(title) if title else None
+
+
+def extract_meta_description_length(meta_description):
+    return len(meta_description) if meta_description else None
+
+
+def extract_h1_length(h1):
+    return len(h1) if h1 else None
+
+
+def extract_h2_length(h2_json):
+    return json.dumps([len(i) for i in json.loads(h2_json)])
+
+
+def extract_count_paragraphs(soup):
+    return len(soup.find_all('h2'))
+
+
+def extract_response_time(response):
+    return response.elapsed.total_seconds()
+
+
+def extract_status(response):
+    return response.status_code
+
+
+def extract_word_count(soup):
+    for s in soup(['script', 'style']):
+        s.decompose()
+    words = re.findall(r'\w+', soup.get_text())
+    return len(words)
+
+
+def extract_page_size(response):
+    return len(response.content)
+
+
+def extract_text_ratio(word_count, page_size):
+    return word_count / page_size
+
+
+def extract_canonical_url(soup):
+    canonical_tag = soup.find('link', attrs={'rel': 'canonical'})
+    return canonical_tag['href'] if canonical_tag else None
+
+
+def extract_meta_robots(soup):
+    meta_robots_tag = soup.find('meta', attrs={'name': 'robots'})
+    return meta_robots_tag['content'] if meta_robots_tag else None
+
+
+def extract_image_alt_attributes(soup):
+    return json.dumps([img['alt'] if 'alt' in img.attrs else None for img in soup.find_all('img')])
+
+
+def extract_structured_data(soup):
+    return json.dumps([script.string for script in soup.find_all('script', attrs={'type': 'application/ld+json'})])
+
+
+def extract_broken_links(soup, url):
+    # You can implement additional logic to identify broken links here
+    return []
+
+
+def extract_language_tags(soup):
+    return soup.html['lang'] if 'lang' in soup.html.attrs else None
